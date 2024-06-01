712fb2b52e7d9f80d32623088ea373dcd26096dc
Quailia <nottakenn@gmail.com>
2024-06-02 03:53:22 +0400

    App 2 of assessment 2: a 'coffee shop' app

--- app/src/main/java/com/example/coffeeshop/MainActivity.kt
+++ app/src/main/java/com/example/coffeeshop/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.coffeeshop
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +18,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ShopActivity::class.java)
             startActivity(intent)
         }
+
+        val infoButton = findViewById<ImageButton>(R.id.btnInfo)
+        infoButton.setOnClickListener { showInfoDialog() }
+    }
+    private fun showInfoDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("About Sweet Café")
+            .setMessage("Welcome to the Sweet Café app! Here you can browse and customize your favorite coffee drinks, add them to your basket, and place an order. Enjoy your coffee today.")
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
